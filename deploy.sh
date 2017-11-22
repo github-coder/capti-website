a0cf3f969771b6d690a844ba39468ff858dbc452
aleksey.severnyuk <aleksey.severnyuk@uvoteam.com>
2017-11-23 00:19:49 +0200

    fix

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ cp -R docs $HOME/docs
 #go to home and setup git
 cd $HOME
 git config --global user.email "sdhnik@gmail.com"
-git config --global user.name "sdhnik"
+git config --global user.name "sdhnik@gmail.com"
 
 #using token clone gh-pages branch
 git clone --quiet --branch=gh-pages https://github.com/github-coder/capti-website.git gh-pages > /dev/null
